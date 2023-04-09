d4ba1a60294aedcc6a4a816635c2995c8efe20ce
hassan.ali <hassan.ali.36900@outlook.com>
2023-04-09 13:56:07 +0300

    adding the list of patients to the class History

--- src/Model/GetExpectation.java
+++ src/Model/GetExpectation.java
@@ -1,7 +1,9 @@
 package Model;
 
 // performing the algorithm needed to achieve our goal
-public class GetExpectation {
+
+public class GetExpectation extends Distance_1 {
 // patients' data
+
 // list of k patients is the nearest among all patients in an instance of history (using some similarity methods)
 }
