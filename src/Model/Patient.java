ba0f130152a28031943d9cbfc904dad1acf8682e
hassan.ali <hassan.ali.36900@outlook.com>
2023-04-09 13:31:08 +0300

    add the internal fields of the class Patient , then encapsulate them

--- src/Model/Patient.java
+++ src/Model/Patient.java
@@ -1,7 +1,35 @@
 package Model;
+
+import java.util.ArrayList;
+
 //express some patient's data
-public class Patient
-{
+public class Patient {
     //list of 7 double values (Medical Test Results)
+
+    private ArrayList<Double> testResult;
+
     //a string determining patient's status
+    private String Statue;
+
+
+    public ArrayList<Double> getTestResult() {
+        return testResult;
+    }
+
+    public void setTestResult(ArrayList<Double> testResult) {
+        this.testResult = testResult;
+    }
+
+    public String getStatue() {
+        return Statue;
+    }
+
+    public void setStatue(String statue) {
+        Statue = statue;
+    }
+
+    public Patient() {
+        setTestResult(new ArrayList<>(7));
+    }
+
 }
