d38a57d62c93f1e1c9811d7e57d991c362a824b4
hassan.ali <hassan.ali.36900@outlook.com>
2023-04-09 13:12:47 +0300

    add method calcSimilarity to the interface

--- src/Model/Distance_Infinity.java
+++ src/Model/Distance_Infinity.java
@@ -1,6 +1,6 @@
 package Model;
-import Interface.ISimilarity;
-public class Dinstance_infinity implements  Interface.ISimilarity {
+
+public class Distance_Infinity implements Interface.ISimilarity {
     @Override
     public double calcSimilarity(Patient x, Patient y) {
         return 0;
