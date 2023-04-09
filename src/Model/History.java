2fefcb702fd7cea9fb9596899370e61dcdedcb61
hassan.ali <hassan.ali.36900@outlook.com>
2023-04-09 13:53:13 +0300

    adding the list of patients to the class History

--- src/Model/History.java
+++ src/Model/History.java
@@ -1,5 +1,14 @@
 package Model;
+
+import java.util.LinkedList;
+
 //expresses all patients' data that are dealt with to generate expectations
 public class History {
     //linked list of patients
+    LinkedList<Patient> Patients;
+
+    History() {
+        Patients = new LinkedList<>();
+    }
+
 }
