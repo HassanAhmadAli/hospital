1789be40c31cc19b572b1550aca48e9271781c0f
hassan.ali <hassan.ali.36900@outlook.com>
2023-04-21 16:29:29 +0300

    replace Double with double

--- src/Model/Distance_Infinity.java
+++ src/Model/Distance_Infinity.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Distance_Infinity implements Interface.ISimilarity {
     @Override
     public double calcSimilarity(Patient x, Patient y) {
-        Double res = 0.0;
+        double res = 0.0;
         ArrayList<Double> x_list = x.getTestResult();
         ArrayList<Double> y_list = y.getTestResult();
         for (int i = 0; i < 7; i++) {
