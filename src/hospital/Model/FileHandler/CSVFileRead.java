f290fc69d2509ea9ec35b51d91ea12fa41c4c863
hassan.ali <hassan.ali.36900@outlook.com>
2023-04-23 09:41:22 +0300

    implementing a csv reader

--- src/hospital/Model/FileHandler/CSVFileRead.java
+++ src/hospital/Model/FileHandler/CSVFileRead.java
@@ -1,5 +1,28 @@
 package hospital.Model.FileHandler;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 // performing operations needed to read data from a file and store it in an instance of History
 public class CSVFileRead {
+    public static void main(String[] args) {
+        String file = "example.csc";
+        BufferedReader reader = null;
+        String line = "";
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+                String[] row = line.split(",");
+                for (String index : row) {
+                    System.out.print("%-10s");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
 
+        } finally {
+
+        }
+
+    }
 }
